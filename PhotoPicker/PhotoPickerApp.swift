749a88fc2e47e9955f67e924c2b409b3791b7d4d
Winsome Tang <tangwins@sheridancollege.ca>
2024-07-22 15:21:34 -0400

    Initial Commit

---
+++ PhotoPicker/PhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerApp.swift
+//  PhotoPicker
+//
+//  Created by Winsome Tang on 2024-07-22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
